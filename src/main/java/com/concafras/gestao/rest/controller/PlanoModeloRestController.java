d1a5a31dbf2b9ec68c676ae54236f032f68e8f94
Marcelo Ferreira da Silva <marcelo@mythe.com.br>
2018-02-19 01:35:24 -0300

    Add planomodelo rest controller

--- src/main/java/com/concafras/gestao/rest/controller/PlanoModeloRestController.java
+++ src/main/java/com/concafras/gestao/rest/controller/PlanoModeloRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,8 +17,8 @@ import com.concafras.gestao.service.MetasInstitutoService;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+@Controller
 public class PlanoModeloRestController {
-
 
   @Autowired
   private MetasInstitutoService metasInstitutoService;
