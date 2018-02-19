b190d95f5fd5b4c0dc1da4bedd3316cd61d80aa5
Marcelo Ferreira da Silva <marcelo@mythe.com.br>
2018-02-18 22:01:15 -0300

    Adjust roles on rest

--- src/main/java/com/concafras/gestao/rest/controller/UsuarioRestController.java
+++ src/main/java/com/concafras/gestao/rest/controller/UsuarioRestController.java
@@ -14,6 +14,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -84,7 +85,14 @@ public class UsuarioRestController {
       
       Usuario usuario = usuarioService.findByUsername(username);
       
+      List<String> listaRoles = new ArrayList<String>();
+
+      for (GrantedAuthority auth : userDetails.getAuthorities()) {
+        listaRoles.add(auth.getAuthority());
+      }
+      
       ResultAuthentication result = new ResultAuthentication();
+      result.setAuthorities(listaRoles);
       result.setSuccess(true);
       result.setUsuario(new UsuarioVO(usuario));
       result.getUsuario().setToken(token.getToken());
--- src/main/java/com/concafras/gestao/rest/model/ResultAuthentication.java
+++ src/main/java/com/concafras/gestao/rest/model/ResultAuthentication.java
@@ -1,9 +1,12 @@
 package com.concafras.gestao.rest.model;
 
+import java.util.List;
+
 import com.concafras.gestao.form.UsuarioVO;
 
 public class ResultAuthentication extends ResultBase {
   private UsuarioVO usuario;
+  private List<String> authorities;
 
   public ResultAuthentication() {
 
@@ -19,6 +22,14 @@ public class ResultAuthentication extends ResultBase {
   
   public void setUsuario(UsuarioVO usuario) {
     this.usuario = usuario;      
+  }
+
+  public void setAuthorities(List<String> listaRoles) {
+	this.authorities = listaRoles;
+  }
+  
+  public List<String> getAuthorities() {
+	return authorities;
   }
 
 }
