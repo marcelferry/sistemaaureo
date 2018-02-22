0005488d327e947f28f9fa7598b74b4c3bac80fd
Marcelo Ferreira da Silva <marcelo@mythe.com.br>
2018-02-22 14:24:09 -0300

    Add status on planoform and fix rodizio data

--- src/main/java/com/concafras/gestao/rest/controller/UsuarioRestController.java
+++ src/main/java/com/concafras/gestao/rest/controller/UsuarioRestController.java
@@ -26,9 +26,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.concafras.gestao.form.EntidadeOptionForm;
 import com.concafras.gestao.form.LoginHistoryVO;
+import com.concafras.gestao.form.RodizioVO;
 import com.concafras.gestao.form.UsuarioVO;
 import com.concafras.gestao.model.Entidade;
 import com.concafras.gestao.model.Pessoa;
+import com.concafras.gestao.model.Rodizio;
 import com.concafras.gestao.model.security.LoginHistory;
 import com.concafras.gestao.model.security.Usuario;
 import com.concafras.gestao.rest.model.DatatableResponse;
@@ -37,6 +39,7 @@ import com.concafras.gestao.security.rest.TokenTransfer;
 import com.concafras.gestao.security.rest.TokenUtils;
 import com.concafras.gestao.service.EntidadeService;
 import com.concafras.gestao.service.LoginHistoryService;
+import com.concafras.gestao.service.RodizioService;
 import com.concafras.gestao.service.UsuarioService;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -44,143 +47,146 @@ import com.google.gson.GsonBuilder;
 @Controller
 public class UsuarioRestController {
 
-  @Autowired
-  UsuarioService usuarioService;
-  
-  @Autowired
-  EntidadeService entidadeService;
-  
-  @Autowired
-  LoginHistoryService loginHistoryService;
+	@Autowired
+	UsuarioService usuarioService;
 
-  @Autowired
-  @Qualifier("customUserDetailsService")
-  private UserDetailsService userDetaislService;
+	@Autowired
+	EntidadeService entidadeService;
 
-  @Autowired
-  @Qualifier("authenticationManager")
-  private AuthenticationManager authManager;
+	@Autowired
+	RodizioService rodizioService;
 
-  /**
-   * Authenticates a user and creates an authentication token.
-   *
-   * @param username
-   *          The name of the user.
-   * @param password
-   *          The password of the user.
-   * @return A transfer containing the authentication token.
-   */
-  @RequestMapping(value = "/api/v1/usuarios/authenticate", method = RequestMethod.POST, produces = "application/json; charset=utf-8")
-  public ResponseEntity<?>  authenticate(@RequestParam("username") String username,
-      @RequestParam("password") String password) {
-    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-        username, password);
-    
-    try{
-      Authentication authentication = authManager
-          .authenticate(authenticationToken);
-      SecurityContextHolder.getContext().setAuthentication(authentication);
-  
-      /*
-       * Reload user as password of authentication principal will be null
-       * after authorization and password is needed for token generation
-       */
-      UserDetails userDetails = userDetaislService.loadUserByUsername(username);
-      
-      TokenTransfer token = new TokenTransfer(TokenUtils.createToken(userDetails));
-      
-      Usuario usuario = usuarioService.findByUsername(username);
-      
-      List<String> listaRoles = new ArrayList<String>();
+	@Autowired
+	LoginHistoryService loginHistoryService;
 
-      for (GrantedAuthority auth : userDetails.getAuthorities()) {
-        listaRoles.add(auth.getAuthority());
-      }
-      
-      List<EntidadeOptionForm> entidades = new ArrayList<EntidadeOptionForm>();
-      if(listaRoles.contains("ROLE_METAS_PRESIDENTE")) {
-          Pessoa pessoa = usuario.getPessoa();
-          List<Entidade> entidadesLoaded = entidadeService.getEntidade(pessoa);
-          for (Entidade entidade : entidadesLoaded) {
-			entidades.add(new EntidadeOptionForm(entidade));
+	@Autowired
+	@Qualifier("customUserDetailsService")
+	private UserDetailsService userDetaislService;
+
+	@Autowired
+	@Qualifier("authenticationManager")
+	private AuthenticationManager authManager;
+
+	/**
+	 * Authenticates a user and creates an authentication token.
+	 *
+	 * @param username
+	 *            The name of the user.
+	 * @param password
+	 *            The password of the user.
+	 * @return A transfer containing the authentication token.
+	 */
+	@RequestMapping(value = "/api/v1/usuarios/authenticate", method = RequestMethod.POST, produces = "application/json; charset=utf-8")
+	public ResponseEntity<?> authenticate(@RequestParam("username") String username,
+			@RequestParam("password") String password) {
+		UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username,
+				password);
+
+		try {
+			Authentication authentication = authManager.authenticate(authenticationToken);
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+
+			/*
+			 * Reload user as password of authentication principal will be null after
+			 * authorization and password is needed for token generation
+			 */
+			UserDetails userDetails = userDetaislService.loadUserByUsername(username);
+
+			TokenTransfer token = new TokenTransfer(TokenUtils.createToken(userDetails));
+
+			Usuario usuario = usuarioService.findByUsername(username);
+
+			List<String> listaRoles = new ArrayList<String>();
+
+			for (GrantedAuthority auth : userDetails.getAuthorities()) {
+				listaRoles.add(auth.getAuthority());
+			}
+
+			List<EntidadeOptionForm> entidades = new ArrayList<EntidadeOptionForm>();
+			if (listaRoles.contains("ROLE_METAS_PRESIDENTE")) {
+				Pessoa pessoa = usuario.getPessoa();
+				List<Entidade> entidadesLoaded = entidadeService.getEntidade(pessoa);
+				for (Entidade entidade : entidadesLoaded) {
+					entidades.add(new EntidadeOptionForm(entidade));
+				}
+			}
+
+			Rodizio rodizio = rodizioService.findByAtivoTrue();
+
+			ResultAuthentication result = new ResultAuthentication();
+			result.setAuthorities(listaRoles);
+			result.setEntidades(entidades);
+			result.setCiclo(new RodizioVO(rodizio));
+			result.setUsuario(new UsuarioVO(usuario));
+			result.setToken(token.getToken());
+			result.setSuccess(true);
+
+			return new ResponseEntity<ResultAuthentication>(result, HttpStatus.OK);
+		} catch (BadCredentialsException ex) {
+			ResultAuthentication result = new ResultAuthentication();
+			result.setSuccess(false);
+			result.setError("E-mail/Senha invalídos");
+
+			return new ResponseEntity<ResultAuthentication>(result, HttpStatus.OK);
 		}
-      }
-      
-      ResultAuthentication result = new ResultAuthentication();
-      result.setAuthorities(listaRoles);
-      result.setEntidades(entidades);
-      result.setSuccess(true);
-      result.setUsuario(new UsuarioVO(usuario));
-      result.getUsuario().setToken(token.getToken());
-  
-      return new ResponseEntity<ResultAuthentication>(result , HttpStatus.OK );
-    }catch(BadCredentialsException ex){
-      ResultAuthentication result = new ResultAuthentication();
-      result.setSuccess(false);
-      result.setError("E-mail/Senha invalídos");
-  
-      return new ResponseEntity<ResultAuthentication>(result , HttpStatus.OK );
-    }
-  }
+	}
 
-  @RequestMapping(value = "/api/v1/usuarios/loginhistory", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
-  public @ResponseBody String listaHistoricoLogin(
-      @RequestParam(value = "iDisplayStart", required = false) int iDisplayStart, 
-      @RequestParam(value = "sSearch", required = false) String searchParameter,
-      @RequestParam(value = "iSortCol_0", required = false) String sortCol,
-      @RequestParam(value = "sSortDir_0", required = false) String sortDir,
-      @RequestParam(value = "iDisplayLength", required = false) int pageDisplayLength,
-      HttpServletRequest request)
-      throws IOException {
-    
-    int pageNumber = 1;
-    if (iDisplayStart > 0 )
-      pageNumber = (iDisplayStart / pageDisplayLength) + 1;
+	@RequestMapping(value = "/api/v1/usuarios/loginhistory", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
+	public @ResponseBody String listaHistoricoLogin(
+			@RequestParam(value = "iDisplayStart", required = false) int iDisplayStart,
+			@RequestParam(value = "sSearch", required = false) String searchParameter,
+			@RequestParam(value = "iSortCol_0", required = false) String sortCol,
+			@RequestParam(value = "sSortDir_0", required = false) String sortDir,
+			@RequestParam(value = "iDisplayLength", required = false) int pageDisplayLength, HttpServletRequest request)
+			throws IOException {
 
+		int pageNumber = 1;
+		if (iDisplayStart > 0)
+			pageNumber = (iDisplayStart / pageDisplayLength) + 1;
 
-    List<LoginHistory> retorno = null;
-    
-    int startRange = 0 + (pageDisplayLength * (pageNumber - 1));
-    
-    if(sortCol.equals("0")){
-      sortCol = "loggedIn";
-    } else if(sortCol.equals("1")){
-      sortCol = "nome";
-    } else if(sortCol.equals("2")){
-      sortCol = "username";
-    } else {
-      sortCol = null;
-    }
+		List<LoginHistory> retorno = null;
 
-    Long totalDisplayRecords = loginHistoryService.countListRangeLoginHistory(searchParameter);
-    
-    retorno = loginHistoryService.listRangeLoginHistory(searchParameter, sortCol, sortDir, startRange, pageDisplayLength);
+		int startRange = 0 + (pageDisplayLength * (pageNumber - 1));
 
-    List<LoginHistoryVO> data = new ArrayList<LoginHistoryVO>();
+		if (sortCol.equals("0")) {
+			sortCol = "loggedIn";
+		} else if (sortCol.equals("1")) {
+			sortCol = "nome";
+		} else if (sortCol.equals("2")) {
+			sortCol = "username";
+		} else {
+			sortCol = null;
+		}
 
-    for (LoginHistory loginHistory : retorno) {
-      LoginHistoryVO vo = new LoginHistoryVO();
-      vo.setId(loginHistory.getId());
-      vo.setLoggedIn(loginHistory.getLoggedIn());
-      vo.setUserIp(loginHistory.getUserIp());
-      vo.setStatus(loginHistory.getStatus());
-      UsuarioVO usuario = new UsuarioVO(loginHistory.getUser());
-      vo.setUser(usuario);
-      data.add(vo);
-    }
+		Long totalDisplayRecords = loginHistoryService.countListRangeLoginHistory(searchParameter);
 
-    DatatableResponse<LoginHistoryVO> result = new DatatableResponse<LoginHistoryVO>();
-    result.setiTotalDisplayRecords(data.size());
-    result.setiTotalRecords(totalDisplayRecords.intValue());
-    result.setAaData(data);
-    result.setSuccess(true);
-    
-    Gson gson = new GsonBuilder().setPrettyPrinting().create();
-    
-    String json2 = gson.toJson(result);
-    
-    return json2;
-    
-    
-  }
+		retorno = loginHistoryService.listRangeLoginHistory(searchParameter, sortCol, sortDir, startRange,
+				pageDisplayLength);
+
+		List<LoginHistoryVO> data = new ArrayList<LoginHistoryVO>();
+
+		for (LoginHistory loginHistory : retorno) {
+			LoginHistoryVO vo = new LoginHistoryVO();
+			vo.setId(loginHistory.getId());
+			vo.setLoggedIn(loginHistory.getLoggedIn());
+			vo.setUserIp(loginHistory.getUserIp());
+			vo.setStatus(loginHistory.getStatus());
+			UsuarioVO usuario = new UsuarioVO(loginHistory.getUser());
+			vo.setUser(usuario);
+			data.add(vo);
+		}
+
+		DatatableResponse<LoginHistoryVO> result = new DatatableResponse<LoginHistoryVO>();
+		result.setiTotalDisplayRecords(data.size());
+		result.setiTotalRecords(totalDisplayRecords.intValue());
+		result.setAaData(data);
+		result.setSuccess(true);
+
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+		String json2 = gson.toJson(result);
+
+		return json2;
+
+	}
 }
--- src/main/java/com/concafras/gestao/rest/model/ResultAuthentication.java
+++ src/main/java/com/concafras/gestao/rest/model/ResultAuthentication.java
@@ -3,43 +3,62 @@ package com.concafras.gestao.rest.model;
 import java.util.List;
 
 import com.concafras.gestao.form.EntidadeOptionForm;
+import com.concafras.gestao.form.RodizioVO;
 import com.concafras.gestao.form.UsuarioVO;
 
 public class ResultAuthentication extends ResultBase {
-  private UsuarioVO usuario;
-  private List<String> authorities;
-  private List<EntidadeOptionForm> entidades;
+	private UsuarioVO usuario;
+	private String token;
+	private RodizioVO ciclo;
+	private List<String> authorities;
+	private List<EntidadeOptionForm> entidades;
 
-  public ResultAuthentication() {
+	public ResultAuthentication() {
 
-  }
+	}
 
-  public ResultAuthentication(ResultBase resultBase) {
-    super(resultBase);
-  }
-  
-  public UsuarioVO getUsuario() {
-    return usuario;
-  }
-  
-  public void setUsuario(UsuarioVO usuario) {
-    this.usuario = usuario;      
-  }
+	public ResultAuthentication(ResultBase resultBase) {
+		super(resultBase);
+	}
 
-  public void setAuthorities(List<String> listaRoles) {
-	this.authorities = listaRoles;
-  }
-  
-  public List<String> getAuthorities() {
-	return authorities;
-  }
+	public UsuarioVO getUsuario() {
+		return usuario;
+	}
 
-  public void setEntidades(List<EntidadeOptionForm> entidades) {
-	this.entidades = entidades;
-  }
-  
-  public List<EntidadeOptionForm> getEntidades() {
-	return entidades;
-  }
+	public void setUsuario(UsuarioVO usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getToken() {
+		return token;
+	}
+	
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+	public RodizioVO getCiclo() {
+		return ciclo;
+	}
+	
+	public void setCiclo(RodizioVO ciclo) {
+		this.ciclo = ciclo;
+	}
+
+	public void setAuthorities(List<String> listaRoles) {
+		this.authorities = listaRoles;
+	}
+
+	public List<String> getAuthorities() {
+		return authorities;
+	}
+
+	public void setEntidades(List<EntidadeOptionForm> entidades) {
+		this.entidades = entidades;
+	}
+
+	public List<EntidadeOptionForm> getEntidades() {
+		return entidades;
+	}
 
 }
